e4041a90180c3601ef7f5b68836b6b08a3f9fcd9
JoshuaMaitland <joshuamaitland1999@outlook.com>
2020-10-23 21:02:48 +0100

    Implemented the cleanup feature
    Also it checks to see whenever the subkey is existed or not.

--- FlexHEXRecentFilesCleanup/Main.cs
+++ FlexHEXRecentFilesCleanup/Main.cs
@@ -28,7 +28,22 @@ namespace FlexHEXRecentFilesCleanup
 
         private void btnCleanup_Click(object sender, EventArgs e)
         {
-
+            DialogResult cleanupDialog = MessageBox.Show("Are you sure you want to clean up all of the recent files?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (cleanupDialog == DialogResult.Yes)
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Inv Softworks\\FlexHEX", true))
+                {
+                    try
+                    {
+                        key.DeleteSubKeyTree("Recent File List", true);
+                        MessageBox.Show("FlexHEX Recent Files cleared sucessfully.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("FlexHEX Recent Files was not be able to clear all of the recent files. Maybe it's because you've already cleaned up or you haven't got FlexHEX installed on your computer.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    }
+                }
+            }
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
