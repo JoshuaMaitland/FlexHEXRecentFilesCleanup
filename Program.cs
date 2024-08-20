27a619bfc06755c303b11cac5529b9a99a5b8873
JoshuaMaitland <joshuamaitland1999@outlook.com>
2024-08-20 20:42:25 +0100

    Code added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Win32; // registry related
 
 namespace FlexHEXRecentFilesCleanup
 {
@@ -10,6 +7,29 @@ namespace FlexHEXRecentFilesCleanup
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Are you sure you want to clear all of the recent files?");
+            var keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.Y)
+            {
+                Console.WriteLine(""); // add a blank line to stop the text appearing next to the "Y"
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Inv Softworks\\FlexHEX", true))
+                {
+                    if (key != null)
+                    {
+                        key.DeleteSubKeyTree("Recent File List");
+                        Console.WriteLine("FlexHEX Recent Files was cleared sucessfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The tool was not be able to clear all of the recent files. Maybe it's because you've already cleaned it up or you haven't got FlexHEX installed on your computer.");
+                    }
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
