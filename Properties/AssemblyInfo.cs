a13d25ad6a03ea260dbb3564520be246e4ff3ddb
JoshuaMaitland <joshuamaitland1999@outlook.com>
2024-08-20 20:55:43 +0100

    Updated Assembly Information Removed a space in the copyright Added a description to match the repository's description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FlexHEXRecentFilesCleanup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This console application cleans up your recent files on FlexHEX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlexHEXRecentFilesCleanup")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
